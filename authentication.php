5e2b1077af74f2e34e95ae5a57f72e73be84396a
frankthe-tank <79240499+frankthe-tank@users.noreply.github.com>
2022-03-13 16:06:32 -0700

    this is the code i have so far

---
+++ authentication.php
@@ -0,0 +1,31 @@
+<?php      
+   $host = "127.0.0.1:3308";  
+   $user = "user";  
+   $password = "pass";  
+   $db_name = "comp440project";  
+     
+   $con = mysqli_connect($host, $user, $password, $db_name);  
+   if(mysqli_connect_errno()) {  
+       die("Failed to connect with MySQL: ". mysqli_connect_error());  
+   }    
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+        }  
+        else{  
+            echo '<script>alert("Welcome to Geeks for Geeks")</script>';  
+        }     
+?> 
