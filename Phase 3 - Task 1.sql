21dd983124c3b49ada1dc472410b8c0744483556
TheJTCooper <jeremy.dominguez.215@my.csun.edu>
2022-05-04 10:38:09 -0700

    Update Phase 3 - Task 1.sql

--- Phase 3 - Task 1.sql
+++ Phase 3 - Task 1.sql
@@ -1,11 +1,15 @@
 SELECT username
 FROM comp440project.blog
 WHERE blogID IN (SELECT blogID
-			FROM comp440project.tag
-			WHERE tag = 'X')
+			FROM comp440project.tag_blog
+			WHERE tagID IN (SELECT tagID
+							FROM comp440project.tag
+                            WHERE tag = 'X'))
 AND blogID IN (SELECT blogID
-			FROM comp440project.tag
-			WHERE tag = 'Y')
+			FROM comp440project.tag_blog
+			WHERE tagID IN (SELECT tagID
+							FROM comp440project.tag
+                            WHERE tag = 'Y'))
 GROUP BY username
 HAVING COUNT(username) >= 2;
 	
